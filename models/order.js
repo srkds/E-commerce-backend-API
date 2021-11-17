51e93c3a9fc30e84154a8838ebf7b5fa87838588
srkds <im.nirav86@gmail.com>
2021-11-17 12:05:16 +0530

    add create order, get all order

--- models/order.js
+++ models/order.js
@@ -24,6 +24,14 @@ const orderSchema = new Schema(
     amount: { type: String },
     address: String,
     updated: Date,
+    /* ENUM : enum can help us to restrict choice 
+    eg: airplane can have only [window, aisle]
+    */
+    status: {
+      type: String,
+      default: "Recieved ",
+      enum: ["canceled", "Delivered", "Shipped", "Processing", "Recieved"],
+    },
     user: {
       type: ObjectId,
       ref: "User",
---
+++ routes/order.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const {
+  getOrderById,
+  createOrder,
+  getAllOrders,
+  getOrderStatus,
+  updateStatus,
+} = require("../controllers/order");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { updateStock } = require("../controllers/product");
+
+const router = express.Router();
+
+// PARAMETER EXTRECTING
+router.params("userId", getUserById);
+router.params("orderId", getOrderById);
+
+// ACTUAL ROUTES
+
+// create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList, // first adding order in purchase list
+  updateStock, // then updating stock
+  createOrder // then creating order
+);
+
+// read
+router.get(
+  "/order/all/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getAllOrders
+);
+
+router.get(
+  "/order/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getOrderStatus
+); // getting order status
+router.put(
+  "/order/:orderId/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateStatus
+); // Updating status of order
+
+module.exports = router;
